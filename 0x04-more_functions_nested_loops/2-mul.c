63ca816483afb7b21698ba4f7b5fa66c55bdb40a
MARWANANO32 <marwanano32@gmail.com>
2023-09-15 00:17:12 +0300

    marwan

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,22 +1,16 @@
 #include "main.h"
 
 /**
- * mul -multiple two integer number1, number2
- * @number1: int the multiplied to number2
- * @number2: int the multiplied ti number1
- * return: the result of the operation
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the result of the operation
  */
-
-int  mul(int number1, int number2)
+int mul(int a, int b)
 {
-	int number1 = 0, number2 = 0, result = 0;
+	int c;
 
-	printf("ENTRY THE FIRST NUMBER : ");
-	scanf("%d", &number1);
-	printf("ENTRY THE SECOND NUMBER : ");
-	scanf("%d", &number2);
-	result = number1 * number2;
+	c = a * b;
 
-	printf("%d * %d = %d\n", number1, number2, result);
-	return (result);
+	return (c);
 }
