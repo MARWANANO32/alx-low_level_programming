dba782c319a2a373e7846573548d211584a10e86
MARWANANO32 <marwanano32@gmail.com>
2023-09-19 01:00:43 +0300

    asd

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-void swap_int(int *a, int *b);
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to first value
+ * @b: pointer to second value
+ */
+void swap_int(int *a, int *b)
 {
 	int c;
 
