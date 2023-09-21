0d1aea6d921dc5f395ffb9b031d38f79e49078c3
MARWANANO32 <marwanano32@gmail.com>
2023-09-21 14:35:07 +0300

    maro

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,29 +7,17 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
-
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
+		n1 = -n;
+		_putchar('-');
 	}
 	else
 	{
-		m = n;
+		n1 = n;
 	}
+	if (n1 / 10)
+		print_number(n1 / 10);
 
-		d = m;
-		count = 1;
-
-		while (d > 9)
-		{
-			d /= 10;
-			count *= 10;
-		}
-
-		for (; count >= 1; count /= 10)
-		{
-			_putchar(((m / count) % 10) + 48);
-		}
+	_putchar((n1 % 10) + '0');
 }
