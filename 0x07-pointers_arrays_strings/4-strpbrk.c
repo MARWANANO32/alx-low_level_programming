07af11fdbd64bc771c3325e7b9057395faed75e1
MARWANANO32 <marwanano32@gmail.com>
2023-09-25 22:18:19 +0300

    MARWAN

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,18 +11,17 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	unsigned int i, j;
 
-	for (i = 0, *s != '\0'; i++)
+	for (i = 0, *(s + i) != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (*s == accept[j])
+			if (*(s + i) == *(accept + j))
 			{
-				return (s);
-				s++;
+				return (s + i);
 			}
 		}
 	}
-	return (NULL);
+	return ('\0');
 }
