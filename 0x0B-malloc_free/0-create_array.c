0b03d4bb4bf3d507b51a93722e1a7e9e571f509f
MARWANANO32 <marwanano32@gmail.com>
2023-10-04 23:02:30 +0300

    MARWAN

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of the array.
+ * @c: storaged char
+ *
+ * Return: an array of chars
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int a;
+	int *i = malloc(size);
+
+	if (size == 0 || i == 0)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; a < size; size--)
+	{
+		i[size] = c;
+	}
+	return (i);
+}
