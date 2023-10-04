81dd1f74b31554588d749d780088e4e8b1e713a9
MARWANANO32 <marwanano32@gmail.com>
2023-10-04 23:35:35 +0300

    MARWAN

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,13 +14,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int a;
 	char *i = malloc(sizeof(c) * size);
 
-	if (size == 0 || i == 0)
+	if (size == 0 || i == NULL)
 	{
 		return (NULL);
 	}
 	for (a = 0; a < size; a++)
 	{
-		i[size] = c;
-		size--;
+		i[a] = c;
+		return (i);
 	}
 }
