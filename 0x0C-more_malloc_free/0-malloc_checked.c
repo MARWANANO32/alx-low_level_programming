62cd235646581b008a946981cf6e181e8afeb972
MARWANANO32 <marwanano32@gmail.com>
2023-10-07 14:25:15 +0300

    MARWAN

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,11 @@
 
 void *malloc_checked(unsigned int b)
 {
-	int *ptr = (int *) malloc(size * sizeof(b));
-	int i;
+	void *ptr = malloc(b);
 
 	if (ptr == NULL)
 	{
-        printf("Memory allocation failed\n");
-        exit(98);
+		exit(98);
 	}
 	return ptr;
 }
