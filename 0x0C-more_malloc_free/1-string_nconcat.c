8a2703ecb630ed7ba76c81cb4d68276f03db1c13
MARWANANO32 <marwanano32@gmail.com>
2023-10-07 17:14:18 +0300

    MARWAN

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,36 +2,49 @@
 #include <stdlib.h>
 
 /**
- * *string_nconcat - to concatenates two strings.
- * @s1: the first string to append to
- * @s2: the second string to concatenate from
- * @n: the number of bytes from s2 to concatenate to s1
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
  *
- * Return: pointer to the resulting string
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, str1 = 0, str2 = 0;
+	char *ptr;
+	unsigned int a, b, c, i;
 
-	char *ptr
+	if (s1 == NULL)
+		s1 = "";
 
-	if (n < str2)
-		s = malloc(sizeof(char) * (str2 + n + 1));
-	else
-		s = malloc(sizeof(char) * (str1 + str2 + 1));
+	if (s2 == NULL)
+		s2 = "";
+
+	for (a = 0; s1[a] != '\0'; a++)
+		;
+	for (b = 0; s2[b] != '\0'; b++)
+		;
+
+	if (n > b)
+		n = b;
+
+	c = a + n;
+
+	ptr = malloc(c + 1);
 
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
-	if (n >= s2)
-	{
-		return (s2);
-	}
-	if (s1 == NULL)
-	{
-		return ("");
-	}
+	for (i = 0; i < c; i++)
+
+	if (i < a)
+		ptr[i] = s1[i];
+
+	else
+		ptr[i] = s2[i - a];
+
+	sout[i] = '\0';
+
 	return (ptr);
 }
