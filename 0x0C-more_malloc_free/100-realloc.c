448eca23f35dcb7d305719b95d1e24d62429c8be
MARWANANO32 <marwanano32@gmail.com>
2023-10-07 20:26:44 +0300

    MARWAN

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,13 +25,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
+	ptr = malloc(new_size);
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	ptr = malloc(new_size);
-
 	if (ptr == NULL)
 	{
 		return (NULL);
