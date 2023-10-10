23ba7631d738483ca0b31a893e2be906f11f6dbc
MARWANANO32 <marwanano32@gmail.com>
2023-10-10 21:57:46 +0300

    marwan

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,11 +14,13 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 		d->name = "(nil)";
+	else
+		printf("age:%f\n", d->age);
 
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 
-	printf("name: %s\nage: %f\nowner: %s\n", d->name, d->age, d->owner);
+	printf("name: %s\nowner: %s\n", d->name, d->owner);
 }
 
 
