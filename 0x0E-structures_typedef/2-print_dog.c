ff1507446a675a1b8ee9858ce555aa62b0b7874b
MARWANANO32 <marwanano32@gmail.com>
2023-10-10 21:29:05 +0300

    marwan

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * *print_dog -  function that prints a struct dog
+ * @d: a pointer
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d ->name == NULL)
+		printf("name%s : nil\n", d->name);
+	if (d ->owner == NULL)
+		printf("owner%s : nil\n", d->owner);
+}
+
+
