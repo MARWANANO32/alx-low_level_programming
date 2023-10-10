c79c618a3dc0321de0682c7fe4f1daa916a6061a
MARWANANO32 <marwanano32@gmail.com>
2023-10-10 19:25:23 +0300

    marwan

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * struct dog - a dog's basic
+ * @name: first member
+ * @age: the second member
+ * @owner: the third member
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
