67be5c6a917fb06dca50af405795802fd9784bdd
MARWANANO32 <marwanano32@gmail.com>
2023-10-13 02:52:50 +0300

    MARWAN

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,41 +1,45 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
-
+#include <stdio.h>
 /**
- * *print_all - function that prints anything
- * @format: a list of types of arguments passed to the function
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ *
+ * Return: no return.
  */
 
 void print_all(const char * const format, ...)
 {
-	va_list(args);
+	va_list(asd);
+	unsigned int i = 0, j, c = 0;
 	char *mar;
 	const char t_arg[] = "cifs";
-	unsigned int i = 0, j, c = 0;
 
-	va_start(args, format);
+	va_start(asd, format);
 	while (format && format[i])
 	{
-		if (format[i] == t_arg[j] && c)
+		j = 0;
+		while (t_arg[j])
+		{
+			if (format[i] == t_arg[j] && c)
 			{
 				printf(", ");
 				break;
 			} j++;
-
+		}
 		switch (format[i])
 		{
-		case 'c':
-			printf("%c", va_arg(args, int)), c = 1;
+			case 'c':
+			printf("%c", va_arg(asd, int)), c = 1;
 			break;
 			case 'i':
-			printf("%d", va_arg(args, int)), c = 1;
+			printf("%d", va_arg(asd, int)), c = 1;
 			break;
 			case 'f':
-			printf("%f", va_arg(args, double)), c = 1;
+			printf("%f", va_arg(asd, double)), c = 1;
 			break;
 			case 's':
-			mar = va_arg(args, char *), c = 1;
+			mar = va_arg(asd, char *), c = 1;
 			if (!mar)
 			{
 				printf("(nil)");
@@ -43,8 +47,7 @@ void print_all(const char * const format, ...)
 			}
 			printf("%s", mar);
 			break;
-		}
-		i++;
+		} i++;
 	}
-	printf("\n"), va_end(args);
+	printf("\n"), va_end(asd);
 }
