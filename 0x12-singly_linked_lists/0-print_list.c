d12e1b5dbd23cdfb76eb96533243c521aea85fda
MARWANANO32 <marwanano32@gmail.com>
2023-10-19 16:20:39 +0300

    marwan

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists"
+#include <stdio.h>
+
+/**
+ * *print_list - prints all the elements
+ * @h: const pointer
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->mul, h->str);
+		}
+
+		h = h->mar;
+		s++;
+	}
+	return (s);
+}
