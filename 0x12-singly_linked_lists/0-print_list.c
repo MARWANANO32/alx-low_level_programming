a82e9bc293c3dee78b4ddc397f78eca16ffd6c7d
MARWANANO32 <marwanano32@gmail.com>
2023-10-19 16:38:06 +0300

    marwan

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,15 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	size_t s = 0;
 
 	while (h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->mul, h->str");
-		h = h->mar;
+			printf("[%u] %s\n", h->len, h->str");
+		h = h->name;
 		n++;
 	}
 	return (n);
