e525ca16396c7f5bee47cce8907bf9907a93c6ab
MARWANANO32 <marwanano32@gmail.com>
2023-10-24 20:55:22 +0300

    MARWAN

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,7 +4,7 @@
  * *listint_len - function that returns the number of elements in a linked
  * @h: the const pointer
  *
- * return: the number of elements
+ * Return: the number of elements
  */
 size_t listint_len(const listint_t *h)
 {
