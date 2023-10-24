c2110f5bf28ebe3b29d3663d8a43660c8aac6189
MARWANANO32 <marwanano32@gmail.com>
2023-10-24 21:52:52 +0300

    MARWAN

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * *add_nodeint_end -  function that adds a new node
+ * n: the const integer number
+ * head: the pointer
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t n = 0;
+
+}
