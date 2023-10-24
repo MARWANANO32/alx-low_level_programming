3c4cdb7c6694105732fefd8cac9e9ec9774b6460
MARWANANO32 <marwanano32@gmail.com>
2023-10-24 21:48:30 +0300

    MARWAN

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * *add_nodeint - function that adds a new node
+ * @n: const int
+ * @head: pointer
+ *
+ * Return:  the address of the new element or NULL
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+
+}
