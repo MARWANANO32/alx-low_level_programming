f2f325b1c82adb4926ccac43e72afb6be9c555ae
MARWANANO32 <marwanano32@gmail.com>
2024-04-11 12:53:44 +0200

    MARWAN MOSTAFA

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -3,35 +3,31 @@
 
 int recursive_search(int *array, size_t size, int value)
 {
-    int i, mid;
+	size_t half = size / 2;
+	size_t i;
 
-    if (size == 0)
-    {
-        return (-1);
-    }
+	if (array == NULL || size == 0)
+		return (-1);
 
-    printf("Searching in array: ");
-    for (i = 0; i < (int)size; i++)
-    {
-        printf("%d", array[i]);
-        if (i < (int)size - 1)
-        {
-            printf(", ");
-        }
-    }
-    printf("\n");
+	printf("Searching in array");
 
-    mid = (size - 1) / 2;
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
 
-    if (array[mid] == value)
-    {
-        return (mid);
-    }
-    else if (array[mid] < value)
-    {
-        return (mid + 1 + recursive_search(array + mid + 1, size - mid - 1, value));
-    }
-    return (recursive_search(array, mid, value));
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
 }
 
 /**
