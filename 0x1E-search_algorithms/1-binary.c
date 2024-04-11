212e601a3018c8f5f79759e051560e24078688b4
MARWANANO32 <marwanano32@gmail.com>
2024-04-11 12:51:57 +0200

    MARWAN MOSTAFA

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,32 +1,6 @@
 #include "search_algos.h"
-/**
- * binary_search - function that searches for a value in a sorted array of integers.
- * @array: pointer to search the first element of the array.
- * @size: The number of the element.
- * @value: The value to search for.
- * Return: the first index where value is located
-*/
 
-int binary_search(int *array, size_t size, int value)
-{
-    int index;
 
-    index = recursive_search(array, size, value);
-
-    if (index >= 0 && array[index] != value)
-    {
-        return (-1);
-    }
-    return (index);
-}
-
-/**
- * recursive_search - function that searches for a value in a sorted array of integers.
- * @array: pointer to search the first element of the array.
- * @size: The number of the element.
- * @value: The value to search for.
- * Return: index of number
-*/
 int recursive_search(int *array, size_t size, int value)
 {
     int i, mid;
@@ -58,4 +32,25 @@ int recursive_search(int *array, size_t size, int value)
         return (mid + 1 + recursive_search(array + mid + 1, size - mid - 1, value));
     }
     return (recursive_search(array, mid, value));
+}
+
+/**
+ * binary_search - function that searches for a value in a sorted array of integers.
+ * @array: pointer to search the first element of the array.
+ * @size: The number of the element.
+ * @value: The value to search for.
+ * Return: the first index where value is located
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+    int index;
+
+    index = recursive_search(array, size, value);
+
+    if (index >= 0 && array[index] != value)
+    {
+        return (-1);
+    }
+    return (index);
 }
